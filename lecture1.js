4ff0f5c3f8714a2f03d9fb7ccb6c6ac958e571f5
nancee1 <nancy.ewurum@outlook.com>
2016-10-25 03:31:45 +0000

    second commit

--- lecture1.js
+++ lecture1.js
@@ -5,15 +5,16 @@
 var firstCar = 'Doge'; //variable names should begin with a letter
 var var = 'variable'; //variable names should make sense and should not be reserved javascript words.
 var firstName = 'Jerry'; //variable names should not contain white space and camelCase is preferred
-var Sch00lName = 'MEST';
-var totalnumber = 100;// camelCase is preferred in javascript
+var schoolName = 'MEST'; // variable name is correct but camleCase is preferred for best practise and readability
+var totalNumber = 100;// variable name is correct but camleCase is preferred for best practise and readability
 
 /*
  * 2. Are the following variable valid? If it is not please explain it and correct them by using the best practise
  */
-var greeting  =  "It's nice to meet you today!";
-var response = "Ashwin said, 'Yes Sir!'"; // in cases where someone needs to be quoted, use a single quote inside a double quote or vice versa
-
+var greeting  =  "It's nice to meet you today!"; //The value of variable is not valid because single quotes are used inside single quotes
+var greeting  =  'It\'s nice to meet you today!'; //Correct thing is to have single quote inside a double quote or vice versa or use the \ escape sequence		
+var response = "Ashwin said, 'Yes Sir!'"; //The value of variable is not valid because single quotes are used inside single quotes
+var response = 'Ashwin said, \'Yes Sir!''; //Correct thing is to have single quote inside a double quote or vice versa or use the \ escape sequence		
 
 /*
  * 3. Try 0.3 + 0.1 === 0.4, explain what happen here
@@ -31,6 +32,13 @@ var response = "Ashwin said, 'Yes Sir!'"; // in cases where someone needs to be 
  	};
 
 
+ var pizza = {}; 														//using dot notation
+	pizza.sliceShape = "triangle";
+	pizza.boxShape = "square";
+	pizza.sliceNum = 12;
+	pizza.calorieSpec = "vegeterian";
+
+
  function Pizza(sliceShape, boxShape, sliceNum, calorieSpec) {         //using object constructor function
     this.sliceShape = sliceShape;
     this.boxShape = boxShape;
@@ -40,6 +48,14 @@ var response = "Ashwin said, 'Yes Sir!'"; // in cases where someone needs to be 
 var pepperoniPizza = new Pizza("triangle", "square", 12, "vegetarian");
 console.log (pepperoniPizza);
 
+
+var pizza = { 															//using object.create
+	sliceShape: "triangle",
+	boxShape: "square",
+	sliceNum: 12,
+	calorieSpec: "vegeterian"
+	};
+var firstPizza = Object.create(pizza);
 
 /*
  *  5. Build a Animal object by using object constructor function, and add at least two method to prototype,
