e6e7bcbb02f2d22938fa82ca68a4fab315590960
nancee1 <nancy.ewurum@outlook.com>
2016-10-25 02:29:50 +0000

    first commit

--- lecture1.js
+++ lecture1.js
@@ -1,18 +1,18 @@
 /*
- * 1. Are the following variable valid? If it is not please explain it and correct them using the est practise
+ * 1. Are the following variable valid? If it is not please explain it and correct them using the best practise
  */
 
-var 1stCar = 'Doge';
-var var = 'variable';
-var first name = 'Jerry';
+var firstCar = 'Doge'; //variable names should begin with a letter
+var var = 'variable'; //variable names should make sense and should not be reserved javascript words.
+var firstName = 'Jerry'; //variable names should not contain white space and camelCase is preferred
 var Sch00lName = 'MEST';
-var totalnumber = 100;
+var totalnumber = 100;// camelCase is preferred in javascript
 
 /*
  * 2. Are the following variable valid? If it is not please explain it and correct them by using the best practise
  */
-var greeting  =  'It's nice to me you today!';
-var response = "Ashwin said, "Yes Sir!"";
+var greeting  =  "It's nice to meet you today!";
+var response = "Ashwin said, 'Yes Sir!'"; // in cases where someone needs to be quoted, use a single quote inside a double quote or vice versa
 
 
 /*
@@ -23,11 +23,30 @@ var response = "Ashwin said, "Yes Sir!"";
 /*
  * 4. Build a pizza object by using all three object creation method, it must has at least 4 properties
  */
+ var pizza = {  														// using object literal notation.
+ 	sliceShape:"triangle",
+ 	boxShape:"square", 
+ 	sliceNum:12, 
+ 	calorieSpec:"vegeterian"
+ 	};
+
+
+ function Pizza(sliceShape, boxShape, sliceNum, calorieSpec) {         //using object constructor function
+    this.sliceShape = sliceShape;
+    this.boxShape = boxShape;
+    this.sliceNum = sliceNum;
+    this.calorieSpec = calorieSpec;
+	}
+var pepperoniPizza = new Pizza("triangle", "square", 12, "vegetarian");
+console.log (pepperoniPizza);
+
 
 /*
  *  5. Build a Animal object by using object constructor function, and add at least two method to prototype,
  *     create two animals from it.
  */
+
+ function Animal()
 
 /*
  * 6. Write a small menu selection function by using if-else and switch, input number, output name of your order
