3f93054c1a74a1ffa438fbe642976ddec9a2d45a
nancee1 <nancy.ewurum@outlook.com>
2016-11-03 05:07:18 +0100

    update lecture 3

--- lecture3.js
+++ lecture3.js
@@ -41,10 +41,21 @@
 /*
  *  3. Write reg expresion for email validation(This is not an easy
  *  question, please make sure you do your research on this).
- *
+ *  reg expression for email validation - 
+      [-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
  */
+ 
 
 /*
  * 4. what is localStorage and sessionStorge under window object? What is the
- *    difference
+ *    difference?
+
+ Local storage stores data until it is explicity deleted. 
+ Data such as username and password of sites frequently visited are stored on local storage so user doesn't have to input them again
+ Session storage stores data for just a session.
+ Data stored in session storage is cleared up when the session ends/browser is closed. 
+
+ The difference is that data stored in localStorage has no expiration time,
+ while data stored in sessionStorage gets cleared when the browsing session ends—that is, when the browser is closed.
  */
+
